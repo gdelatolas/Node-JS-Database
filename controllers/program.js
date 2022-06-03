f0f7827b3ec161ec4986ee373ab2eb0b03561c1c
gdelatolas <gregorydelatolas@gmail.com>
2022-06-03 14:32:16 +0300

    test

--- controllers/program.js
+++ controllers/program.js
@@ -1,3 +1,4 @@
+const { type } = require('express/lib/response');
 const { pool } = require('../utils/database');              //We define pool from ../utils/database
 
 exports.getProgramsByCriteria = (req, res, next) => {
@@ -9,10 +10,63 @@ exports.getProgramsByCriteria = (req, res, next) => {
 
 /*From sumbit we come here, cause sumbit sends a post req*/
 exports.postProgramsByCriteria = (req, res, next) => {
-    const name = req.body.duration;
+    const date = req.body.date;
+    const duration = req.body.duration;
+    const exec_id = req.body.exec_id;
 
-    console.log(name);
-    res.render('programs_by_criteria.ejs', {
-        pageTitle: "QUERY 1",
-    })
+    /*
+    select program.program_id , p.project_id
+    from program join project p
+    on program.program_id = p.program_id
+    where 
+        '2021-01-01' <= p.start_date and
+        duration = 3 and
+        executive_id = 1
+    */
+    var sql="select program.program_id , p.project_id "+
+        "from program join project p "+
+        "on program.program_id = p.program_id "+
+        "where 1=1 ";
+
+    var date_cond = "";
+    
+    if (date !== ''){
+        
+        date_cond=" and start_date>="+date
+    }
+
+    var duration_cond = "";
+    if (typeof duration !== 'undefined'){
+        duration_cond= " and duration ="+duration
+    }
+
+    var exec_id_cond = "";
+    if (exec_id !== ''){
+        exec_id_cond= " and executive_id ="+exec_id
+    }
+
+    sql += (date_cond + duration_cond + exec_id_cond)
+
+    /*We go to the database*/
+    pool.getConnection((err, conn) => {
+        if(err){
+            console.log(err);
+        }
+        // a promise can succeed or fail.
+        conn.promise().query(sql)
+        .then(([rows, fields]) => {
+            res.render('programs_by_criteria.ejs', {
+                pageTitle: "QUERY 1",
+                query_res: rows
+                //messages: messages
+            })
+            //resolve();
+        })
+        .then(() => pool.releaseConnection(conn))
+        .catch(err => console.log(err))
+    });
+
+
+
+    
 }
